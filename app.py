2b31ca6415ce44ac6c50698e98d2211bf647896d
Rishi Premlani <rishipremlani1002@gmail.com>
2026-08-06 00:00:55 +0530

    Update app.run for Render deployment

--- app.py
+++ app.py
@@ -213,5 +213,8 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()
 
-    app.run(debug=True, use_reloader=False)
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000))
+    )
 
